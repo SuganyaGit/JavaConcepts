194b06e3e56e4b13dcb19ebe54222a883ce53421
SuganyaGit <suganya.cons88@gmail.com>
2016-04-27 11:34:38 +0530

    Create HashSetCollection.java

---
+++ Collections/HashSetCollection.java
@@ -0,0 +1,26 @@
+/*
+uses hashtable to store the elements.It extends AbstractSet class and implements Set interface.
+contains unique elements only.
+can have one null element
+
+List can contain duplicate elements whereas Set contains unique elements only.
+*/
+
+import java.util.*;  
+class HashSetCollection{  
+ public static void main(String args[]){  
+   
+  HashSet<String> al=new HashSet<String>();  
+  al.add("Ravi");  
+  al.add("Vijay");  
+  al.add("Ravi");
+  al.add(null);  
+  al.add("Ajay");  
+  al.add(null);  
+  
+  Iterator<String> itr=al.iterator();  
+  while(itr.hasNext()){  
+   System.out.println(itr.next());  
+  }  
+ }  
+} 
