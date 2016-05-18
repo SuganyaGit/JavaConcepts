f77a479aed997e675155e63c2c14ab24f4a68c52
SuganyaGit <suganya.cons88@gmail.com>
2016-05-18 14:28:21 +0530

    Update generics.java

--- Collections/generics.java
+++ Collections/generics.java
@@ -1,3 +1,20 @@
+/*
+
+Before generics, we can store any type of objects in collection i.e. non-generic.
+Now generics, forces the java programmer to store specific type of objects.
+
+Advantage of Java Generics
+
+There are mainly 3 advantages of generics. They are as follows:
+
+1)	Type-safety : We can hold only a single type of objects in generics. It doesn’t allow to store other objects.
+
+2)	Type casting is not required: There is no need to typecast the object.
+
+3)  Compile-Time Checking: It is checked at compile time so problem will not occur at runtime.
+     The good programming strategy says it is far better to handle the problem at compile time than runtime.
+*/
+
 import java.util.*; 
 class generics{
 
